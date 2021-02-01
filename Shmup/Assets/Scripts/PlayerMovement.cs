23ebbbf06ae133d325dab2a28ffc09c8be98c433
FifaGod <vijayakr@sheridancollege.ca>
2021-02-01 13:07:01 -0500

    Implemented Player Movement
    - Added vector based movement to player (all directions)
    - Added texture folder
    - Added a texture to ground
    - Resized the player and camera

--- Shmup/Assets/Scripts/PlayerMovement.cs
+++ Shmup/Assets/Scripts/PlayerMovement.cs
@@ -8,7 +8,19 @@ public class PlayerMovement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W))
         {
-            transform.position += new Vector3(0, 1);
+            transform.position += new Vector3(0, +1);
         }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(0, -1);
+        }
+        if (Input.GetKey(KeyCode.A)){
+            transform.position += new Vector3(-1, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(+1, 0);
+        }
+
     }
 }
