ea8299011cc80de37c53b8aa99615af73f454b97
oplinger-PC\oplinger <willtrav@sheridancollege.ca>
2021-02-01 11:25:49 -0500

    Unrotated Scene, added scrolling behavior

--- Shmup/Assets/Scripts/BackgroundGeneration/BackgroundGenerator.cs
+++ Shmup/Assets/Scripts/BackgroundGeneration/BackgroundGenerator.cs
@@ -6,19 +6,36 @@ public class BackgroundGenerator : MonoBehaviour
 {
     public GameObject backgroundTilePrefab;
     List<GameObject> generatedBackgroundTiles;
+    public float scrollSpeed;
     // Start is called before the first frame update
     void Start()
     {
+        generatedBackgroundTiles = new List<GameObject>();
         GameObject initialTile = Instantiate(backgroundTilePrefab, Vector3.zero, Quaternion.identity);
         float initialTileHeight = Camera.main.orthographicSize * 2;
         float initialTileWidth = initialTileHeight * Screen.width / Screen.height;
         initialTile.transform.localScale =new Vector3(initialTileWidth, initialTileHeight, 1);
-        initialTile.transform.Rotate(90, 0, 0);
+        generatedBackgroundTiles.Add(initialTile);
+        print(generatedBackgroundTiles[0]);
+        StartCoroutine(ScrollBackground(scrollSpeed));
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator ScrollBackground(float scrollspeed)
+    {
+        while (true)
+        {
+            for (int i = 0; i < generatedBackgroundTiles.Count; i++)
+            {
+                generatedBackgroundTiles[i].transform.position += Vector3.down * scrollspeed;
+
+            }
+            yield return new WaitForSeconds(.1f);
+        }
     }
 }
