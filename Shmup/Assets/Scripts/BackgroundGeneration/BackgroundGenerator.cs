f96233a92e594d593916bb1e8e9d1ce919af2d4e
oplinger-PC\oplinger <willtrav@sheridancollege.ca>
2021-02-01 11:00:14 -0500

    Create Background Tile Prefab, Generate initial backgound tile to screen size

---
+++ Shmup/Assets/Scripts/BackgroundGeneration/BackgroundGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundGenerator : MonoBehaviour
+{
+    public GameObject backgroundTilePrefab;
+    List<GameObject> generatedBackgroundTiles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject initialTile = Instantiate(backgroundTilePrefab, Vector3.zero, Quaternion.identity);
+        float initialTileHeight = Camera.main.orthographicSize * 2;
+        float initialTileWidth = initialTileHeight * Screen.width / Screen.height;
+        initialTile.transform.localScale =new Vector3(initialTileWidth, initialTileHeight, 1);
+        initialTile.transform.Rotate(90, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
