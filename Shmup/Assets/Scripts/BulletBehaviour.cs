1b078fbf93a4193268d082b5bf2c66600212b680
braithad <braithad@sheridancollege.ca>
2021-02-08 11:05:13 -0500

    Add basic bullet behaviour

---
+++ Shmup/Assets/Scripts/BulletBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour
+{
+    // Bullet properties
+    public float bltSpd, bltAccel;
+
+    // Get component(s)
+    Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        // Move bullet by factor of bullet speed and bullet acceleration each frame
+        rb.MovePosition(rb.position + (Vector2.up * bltSpd) * Time.deltaTime);
+
+        bltSpd += bltAccel;
+    }
+}
