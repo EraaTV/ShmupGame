9ae7d8236692defd515908e85deec34e82590d17
braithad <braithad@sheridancollege.ca>
2021-02-08 11:15:44 -0500

    Set SampleScene cam to orthographic and edit Hi script

--- Shmup/Assets/Scripts/Hi.cs
+++ Shmup/Assets/Scripts/Hi.cs
@@ -7,12 +7,6 @@ public class Hi : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        print("Hello world!");
     }
 }
