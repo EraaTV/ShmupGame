585a6b35a1b3a75f00d3cc2afc181f9e98e6b33a
braithad <braithad@sheridancollege.ca>
2021-02-26 17:24:25 -0500

    Add enemy prefab/script using scriptable objects
    Using existing scripts for controlling enemy behaviour, a single, simplified
    script for enemy behaviour has been created to make use of newly added
    scriptable objects for different enemy presets.

---
+++ Shmup/Assets/Scripts/Enemy/EnemySO.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Scriptable object for instantiating enemy base stats into spawned prefabs
+[CreateAssetMenu(fileName = "EnemyType", menuName = "ScriptableObjects/EnemyType", order = 1)]
+public class EnemySO : ScriptableObject
+{
+    public string enemyName;
+
+    public float enemyMaxHp = 3, fireRate = 1, enterSpeed = 1, moveSpeed = 1;
+}
