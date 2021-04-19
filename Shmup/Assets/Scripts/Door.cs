1db918bd64b3a5e65da056fa8cd8c70036708e91
RavenVVV <qianwu512@gmail.com>
2021-04-18 17:07:45 -0700

    Implemented Level1

---
+++ Shmup/Assets/Scripts/Door.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour
+{
+    public bool unlocked;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        unlocked = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (unlocked == true)
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
